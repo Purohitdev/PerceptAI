b437e9f11a505495f1005f4a0c9900ec10d4891c
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-09-17 16:55:51 +0530

    acertinity configured --major
    acertinity ui installed and configured

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
+import { BackgroundLines } from './components/ui/background-lines';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [projects, setProjects] = useState([]);
-
-  useEffect(() => {
-    // Fetching the project list from Flask backend
-    axios.get('http://localhost:5000/')
-      .then(response => {
-        setProjects(response.data.projects);  // Set the projects in state
-      })
-      .catch(error => {
-        console.error('There was an error fetching the projects!', error);
-      });
-  }, []);
-
-  // to run a project
-  const runProject = (projectName) => {
-    axios.get(`http://localhost:5000/run/${projectName}`)  
-      .then(response => {
-        alert(response.data.message || 'Success!');  
-      })
-      .catch(error => {
-        alert('There was an error running the project!');  // error handling
-        console.error('Error running the project!', error);
-      });
-  };
-
   return (
-    <div className="App">
-      <h1>Project List</h1>
-      <ul>
-        {projects.map((project, index) => (
-          <li key={index}>
-            {project} 
-            <button onClick={() => runProject(project)} style={{ marginLeft: '10px' }}>
-              Run
-            </button>  
-          </li>
-        ))}
-      </ul>
+    <div>
+      <BackgroundLines />
+      {/* Other components */}
     </div>
   );
 }
