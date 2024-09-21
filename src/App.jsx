680c4e4235572a543007ff3a56ffc797408b4add
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-09-21 13:34:45 +0530

    backend configured --major
    backend connected to frontend
    --projects fetching using cors
    --project execution

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import LandingPage from './Components/Section1/Hero/Hero';
 import React from 'react';
+import RunProjectButton from './Components/Section2/RunProjectButton';
+import ProjectLinks from './Components/Section2/ProjectLinks';
 import './App.css'
 
 function App() {
   return (
     <div>
       <LandingPage />
+      <ProjectLinks />
     </div>
   );
 }
