49ee403be08ba20060a50b38e176bd93215faf5c
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-09-22 00:37:49 +0530

    made logo clickable --minor

--- src/Components/Section1/Navbar/Navbar.jsx
+++ src/Components/Section1/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const FloatingNavbar = () => {
     <nav className="fixed top-4 left-1/2 transform -translate-x-1/2 bg-gray bg-opacity-70 backdrop-blur-md rounded-full shadow-lg border border-gray-700 z-10 p-4 navbar">
       <div className="flex items-center space-x-6">
         {/* Logo */}
-        <img src={logo} alt="PerceptAI Logo" className="h-10" />
+        <a href="#"><img src={logo} alt="PerceptAI Logo" className="h-10" /></a>
         
         {/* Navbar Items */}
         {/* <span className="text-lg font-bold text-white">PerceptAI</span> */}
