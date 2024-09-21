6d38e5005f9df6fcabc6059ba11d2b9836d163c8
Zaid Rakhange <zaidcommits.github@gmail.com>
2024-09-21 14:22:18 +0530

    navbar logo added --minor

--- src/Components/Section1/Navbar/Navbar.jsx
+++ src/Components/Section1/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import logo from '../../../assets/Images/corelogo.png'; // Corrected import
 
 const FloatingNavbar = () => {
   return (
     <nav className="fixed top-4 left-1/2 transform -translate-x-1/2 bg-gray bg-opacity-70 backdrop-blur-md rounded-full shadow-lg border border-gray-700 z-10 p-4">
       <div className="flex items-center space-x-6">
         {/* Logo */}
-        {/* <img src="/path/to/logo.png" alt="PerceptAI Logo" className="h-8" /> */}
+        <img src={logo} alt="PerceptAI Logo" className="h-10" />
         
         {/* Navbar Items */}
-        <span className="text-lg font-bold text-white">PerceptAI</span>
-        <a href="https://github.com/zaid/commits" className="text-gray-300 hover:text-white transition-colors duration-200 ">
+        {/* <span className="text-lg font-bold text-white">PerceptAI</span> */}
+        <a href="#" className="text-gray-300 hover:text-white transition-colors duration-200">
           Home
         </a>
         <a href="#" className="text-gray-300 hover:text-white transition-colors duration-200">
